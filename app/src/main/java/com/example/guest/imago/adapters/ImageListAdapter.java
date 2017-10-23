d4111083415d9180ce982529b75a6aefc835980d
Kate Byars <katebyars5@gmail.com>
2017-10-23 12:29:07 -0700

    add initial program files.

---
+++ app/src/main/java/com/example/guest/imago/adapters/ImageListAdapter.java
@@ -0,0 +1,8 @@
+package com.example.guest.imago.adapters;
+
+/**
+ * Created by Guest on 10/23/17.
+ */
+
+public class ImageListAdapter {
+}
---
+++ app/src/main/java/com/example/guest/imago/models/Image.java
@@ -0,0 +1,8 @@
+package com.example.guest.imago.models;
+
+/**
+ * Created by Guest on 10/23/17.
+ */
+
+public class Image {
+}
