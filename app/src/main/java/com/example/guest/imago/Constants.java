f23d54c209cf4fa50a596ecbbdc8e979238b887c
Kate Byars <katebyars5@gmail.com>
2017-10-23 13:49:22 -0700

    correct service logic.

--- app/src/main/java/com/example/guest/imago/Constants.java
+++ app/src/main/java/com/example/guest/imago/Constants.java
@@ -1,6 +1,6 @@
-//package com.example.guest.imago;
-//public class Constants {
-//    public static final String UNSPLASH_TOKEN = BuildConfig.UNSPLASH_TOKEN;
-//    public static final String UNSPLASH_BASE_URL = "https://api.unsplash.com/search/users";
-//    public static final String UNSPLASH_LOCATION_QUERY_PARAMETER = "query";
-//}
+package com.example.guest.imago;
+public class Constants {
+    public static final String UNSPLASH_TOKEN = BuildConfig.UNSPLASH_TOKEN;
+    public static final String UNSPLASH_BASE_URL = "https://api.unsplash.com/search/photos";
+    public static final String UNSPLASH_QUERY_PARAMETER = "query";
+}
--- app/src/main/java/com/example/guest/imago/ui/ImageListActivity.java
+++ app/src/main/java/com/example/guest/imago/ui/ImageListActivity.java
@@ -1,68 +1,69 @@
-//package com.example.guest.imago.ui;
-//import android.content.Intent;
-//import android.support.v7.app.AppCompatActivity;
-//import android.os.Bundle;
-//import android.support.v7.widget.LinearLayoutManager;
-//import android.support.v7.widget.RecyclerView;
-//
-//import com.example.guest.imago.adapters.ImageListAdapter;
-//
-//import java.io.IOException;
-//import java.util.ArrayList;
-//
-//import butterknife.Bind;
-//import butterknife.ButterKnife;
-//import okhttp3.Call;
-//import okhttp3.Callback;
-//import okhttp3.Response;
-//
-//public class ImageListActivity extends AppCompatActivity {
-//    public static final String TAG = ImageListActivity.class.getSimpleName();
-//
-//    @Bind(R.id.recyclerView) RecyclerView mRecyclerView;
-//    private ImageListAdapter mAdapter;
-//
-//    public ArrayList<Image> mImages = new ArrayList<>();
-//
-//    @Override
-//    protected void onCreate(Bundle savedInstanceState) {
-//        super.onCreate(savedInstanceState);
-//        setContentView(R.layout.activity_images);
-//        ButterKnife.bind(this);
-//
-//        Intent intent = getIntent();
-//        String location = intent.getStringExtra("location");
-//
-//        getRestaurants(location);
-//    }
-//
-//    private void getRestaurants(String location) {
-//        final UnsplashService unsplashService = new UnsplashService();
-//
-//        unsplashService.findImages(search, new Callback() {
-//
-//            @Override
-//            public void onFailure(Call call, IOException e) {
-//                e.printStackTrace();
-//            }
-//
-//            @Override
-//            public void onResponse(Call call, Response response) {
-//                mImages = unsplashService.processResults(response);
-//
-//                ImageListActivity.this.runOnUiThread(new Runnable() {
-//
-//                    @Override
-//                    public void run() {
-//                        mAdapter = new ImageListAdapter(getApplicationContext(), mImages);
-//                        mRecyclerView.setAdapter(mAdapter);
-//                        RecyclerView.LayoutManager layoutManager =
-//                                new LinearLayoutManager(ImageListActivity.this);
-//                        mRecyclerView.setLayoutManager(layoutManager);
-//                        mRecyclerView.setHasFixedSize(true);
-//                    }
-//                });
-//            }
-//        });
-//    }
-//}
+package com.example.guest.imago.ui;
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+
+import com.example.guest.imago.adapters.ImageListAdapter;
+import com.example.guest.imago.models.Image;
+
+import java.io.IOException;
+import java.util.ArrayList;
+
+import butterknife.Bind;
+import butterknife.ButterKnife;
+import okhttp3.Call;
+import okhttp3.Callback;
+import okhttp3.Response;
+
+public class ImageListActivity extends AppCompatActivity {
+    public static final String TAG = ImageListActivity.class.getSimpleName();
+
+    @Bind(R.id.recyclerView) RecyclerView mRecyclerView;
+    private ImageListAdapter mAdapter;
+
+    public ArrayList<Image> mImages = new ArrayList<>();
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_images);
+        ButterKnife.bind(this);
+
+        Intent intent = getIntent();
+        String location = intent.getStringExtra("location");
+
+        getRestaurants(location);
+    }
+
+    private void getRestaurants(String location) {
+        final UnsplashService unsplashService = new UnsplashService();
+
+        unsplashService.findImages(search, new Callback() {
+
+            @Override
+            public void onFailure(Call call, IOException e) {
+                e.printStackTrace();
+            }
+
+            @Override
+            public void onResponse(Call call, Response response) {
+                mImages = unsplashService.processResults(response);
+
+                ImageListActivity.this.runOnUiThread(new Runnable() {
+
+                    @Override
+                    public void run() {
+                        mAdapter = new ImageListAdapter(getApplicationContext(), mImages);
+                        mRecyclerView.setAdapter(mAdapter);
+                        RecyclerView.LayoutManager layoutManager =
+                                new LinearLayoutManager(ImageListActivity.this);
+                        mRecyclerView.setLayoutManager(layoutManager);
+                        mRecyclerView.setHasFixedSize(true);
+                    }
+                });
+            }
+        });
+    }
+}
