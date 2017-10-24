ecb50a3af3b7f5d2779af179cbbdb946e97178b3
Kate Byars <katebyars5@gmail.com>
2017-10-24 12:00:41 -0700

    add a bottom nav bar to go to saved items and profile.

--- app/src/main/java/com/example/guest/imago/ui/MainActivity.java
+++ app/src/main/java/com/example/guest/imago/ui/MainActivity.java
@@ -1,16 +1,25 @@
 package com.example.guest.imago.ui;
+import android.support.annotation.NonNull;
+import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.content.Intent;
 import android.graphics.Typeface;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.support.design.internal.BottomNavigationItemView;
+import android.support.design.widget.BottomNavigationView;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
 
 import com.example.guest.imago.R;
+import com.example.guest.imago.fragments.ProfileFragment;
+import com.example.guest.imago.fragments.SavedFragment;
 
 import butterknife.Bind;
 import butterknife.ButterKnife;
@@ -18,8 +27,42 @@ import butterknife.ButterKnife;
 //import static com.example.guest.imago.R.id.appNameTextView;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+    private BottomNavigationView navigation;
+    private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener = new BottomNavigationView.OnNavigationItemSelectedListener() {
+
+        @Override
+        public boolean onNavigationItemSelected(@NonNull MenuItem item){
+            switch (item.getItemId()) {
+                case R.id.profile:
+                    setTitle("Profile");
+                    ProfileFragment profileFragment = new ProfileFragment();
+                    FragmentTransaction fragmentTransaction1 = getSupportFragmentManager().beginTransaction();
+                    fragmentTransaction1.replace(R.id.FragmentFrame, profileFragment, "FragmentName");
+                    fragmentTransaction1.commit();
+
+                    return true;
+
+                case R.id.saved:
+                    setTitle("Saved");
+                    SavedFragment savedFragment = new SavedFragment();
+                    FragmentTransaction fragmentTransaction2 = getSupportFragmentManager().beginTransaction();
+                    fragmentTransaction2.replace(R.id.FragmentFrame, savedFragment, "FragmentName");
+                    fragmentTransaction2.commit();
+                    return true;
+
+//                case R.id.favorites:
+//                    setTitle("Favorites");
+//                    FavoritesFragment favoritesFragment = new FavoritesFragment();
+//                    FragmentTransaction fragmentTransaction3 = getSupportFragmentManager().beginTransaction();
+//                    fragmentTransaction3.replace(R.id.FragmentFrame, favoritesFragment, "FragmentName");
+//                    fragmentTransaction3.commit();
+//                    return true;
+            }
+            return false;
+        }
+    };
+
     @Bind(R.id.findImagesButton) Button mFindImagesButton;
-//    @Bind(R.id.appNameTextView) TextView mAppNameTextView;
     @Bind(R.id.searchEditText) EditText mSearchEditText;
 
     @Override
@@ -31,10 +74,10 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         Typeface latoItalic = Typeface.createFromAsset(getAssets(), "fonts/Lato-Italic.ttf");
         mSearchEditText.setTypeface(latoItalic);
 
-//        Typeface latoLight = Typeface.createFromAsset(getAssets(), "fonts/Lato-Light.ttf");
-//        mAppNameTextView.setTypeface(latoLight);
-
         mFindImagesButton.setOnClickListener(this);
+
+        BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.bottom_navigation);
+        navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
     }
 
     @Override
