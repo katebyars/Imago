258dbc594f0623cd0ed0213f03fb0ef6908118af
Guest User <Guest@Epicodus-3D.local>
2017-11-01 15:19:58 -0700

    add some interface logic.

--- app/src/main/java/com/example/guest/imago/ui/ImageListActivity.java
+++ app/src/main/java/com/example/guest/imago/ui/ImageListActivity.java
@@ -15,6 +15,7 @@ import com.example.guest.imago.R;
 import com.example.guest.imago.adapters.ImageListAdapter;
 import com.example.guest.imago.models.Image;
 import com.example.guest.imago.services.UnsplashService;
+import com.example.guest.imago.util.OnImageSelectedListener;
 import com.squareup.picasso.Picasso;
 
 import java.io.IOException;
@@ -26,14 +27,23 @@ import okhttp3.Call;
 import okhttp3.Callback;
 import okhttp3.Response;
 
-public class ImageListActivity extends AppCompatActivity {
+public class ImageListActivity extends AppCompatActivity implements OnImageSelectedListener {
 
+    private Integer mPosition;
+    ArrayList<Image> mImages;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_images);
+
+
     }
 
+    @Override
+    public void onImageSelected(Integer position, ArrayList<Image> images) {
+        mPosition = position;
+        mImages = images;
+    }
 
 }
