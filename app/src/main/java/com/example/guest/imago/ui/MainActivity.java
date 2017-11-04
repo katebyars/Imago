668f3864c749dcdd71cbcdcf498eb019f32657f5
Guest User <Guest@Epicodus-3A.local>
2017-11-04 13:18:07 -0700

    add in logout to main menu.

--- app/src/main/java/com/example/guest/imago/ui/MainActivity.java
+++ app/src/main/java/com/example/guest/imago/ui/MainActivity.java
@@ -54,6 +54,30 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         mSavedImagesButton.setOnClickListener(this);
     }
 
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu_main, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+        if (id == R.id.action_logout) {
+            logout();
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    private void logout() {
+        FirebaseAuth.getInstance().signOut();
+        Intent intent = new Intent(MainActivity.this, LoginActivity.class);
+        intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+        startActivity(intent);
+        finish();
+    }
 
     @Override
     public void onClick(View v) {
